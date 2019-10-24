f1acafcf5cf6ab70d1e199308de4e2691d67b8e1
Chad Metcalf <metcalfc@gmail.com>
2019-10-23 21:49:35 -0700

    Moving to goreleaser for releases.
    Local dev will stay in the containers and goreleaser can handle all the
    niceities for GitHub releases.

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import (
 	"github.com/fatih/color"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
-	color.Red("Hello World")
+	color.Red("Hello World.")
+	color.Blue("version: %s, commit: %s, date: %s", version, commit, date)
 }
